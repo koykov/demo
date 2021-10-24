7a5625307005d4337653634632a7e7e88c3b4cbe
Konstantin Voykov <koykov0@gmail.com>
2021-10-24 14:58:17 +0300

    Remove ForceSet config param.

--- cbytecache/repo.go
+++ cbytecache/repo.go
@@ -10,7 +10,6 @@ type RequestInit struct {
 	Buckets        uint                  `json:"buckets"`
 	Expire         time.Duration         `json:"expire_ns"`
 	Vacuum         time.Duration         `json:"vacuum_ns"`
-	ForceSet       bool                  `json:"force_set"`
 	CollisionCheck bool                  `json:"collision_check"`
 	MaxSize        cbytecache.MemorySize `json:"max_size"`
 	MetricsKey     string                `json:"metrics_key"`
@@ -23,7 +22,6 @@ func (r *RequestInit) MapConfig(conf *cbytecache.Config) {
 	conf.Buckets = r.Buckets
 	conf.Expire = r.Expire
 	conf.Vacuum = r.Vacuum
-	conf.ForceSet = r.ForceSet
 	conf.CollisionCheck = r.CollisionCheck
 	conf.MaxSize = r.MaxSize
 }
