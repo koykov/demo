47d437bfa487015e3e884100ef2d4969dd539bd6
Konstantin Voykov <koykov0@gmail.com>
2021-11-29 23:13:18 +0200

    Allow specifying schedule on init.

--- blqueue/repo.go
+++ blqueue/repo.go
@@ -8,20 +8,29 @@ import (
 
 type RequestInit struct {
 	Size      uint64        `json:"size"`
-	Workers   uint32        `json:"workers"`
-	Heartbeat time.Duration `json:"heartbeat"`
+	Workers   uint32        `json:"workers,omitempty"`
+	Heartbeat time.Duration `json:"heartbeat,omitempty"`
 
 	WorkersMin   uint32  `json:"workers_min"`
 	WorkersMax   uint32  `json:"workers_max"`
-	WorkerDelay  uint32  `json:"worker_delay"`
-	WakeupFactor float32 `json:"wakeup_factor"`
-	SleepFactor  float32 `json:"sleep_factor"`
+	WorkerDelay  uint32  `json:"worker_delay,omitempty"`
+	WakeupFactor float32 `json:"wakeup_factor,omitempty"`
+	SleepFactor  float32 `json:"sleep_factor,omitempty"`
 
 	ProducersMin  uint32 `json:"producers_min"`
 	ProducersMax  uint32 `json:"producers_max"`
-	ProducerDelay uint32 `json:"producer_delay"`
+	ProducerDelay uint32 `json:"producer_delay,omitempty"`
 
-	AllowLeak bool `json:"allow_leak"`
+	AllowLeak bool `json:"allow_leak,omitempty"`
+
+	Schedule []struct {
+		Range        string  `json:"range,omitempty"`
+		RelRange     string  `json:"rel_range,omitempty"`
+		WorkersMin   uint32  `json:"workers_min,omitempty"`
+		WorkersMax   uint32  `json:"workers_max,omitempty"`
+		WakeupFactor float32 `json:"wakeup_factor,omitempty"`
+		SleepFactor  float32 `json:"sleep_factor,omitempty"`
+	} `json:"schedule,omitempty"`
 }
 
 func (r *RequestInit) MapConfig(conf *blqueue.Config) {
