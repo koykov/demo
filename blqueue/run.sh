4462679e5537ca5723c9e1c517a46423e12b03d1
Konstantin Voykov <koykov0@gmail.com>
2021-12-19 16:27:39 +0200

    blqueue: build log improvements.

--- blqueue/run.sh
+++ blqueue/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 go mod tidy
-go build -o $GOPATH/bin/blqueued . &> logs/build.log
+go build -a -v -o $GOPATH/bin/blqueued . &> logs/build.log
 LOG="logs/$(date +%Y.%m.%d_%H:%M).log"
 $GOPATH/bin/blqueued &> $LOG
