c85b98bfea52a2b367a757aa8ea0a8da4788a5e0
Konstantin Voykov <koykov0@gmail.com>
2022-03-10 15:05:52 +0200

    traceID: add traceID fields to models.

--- traceID/model/request.go
+++ traceID/model/request.go
@@ -8,23 +8,26 @@ import (
 )
 
 type Request struct {
-	BF    float64 `json:"bf"`
-	BC    float64 `json:"bc"`
-	Limit uint    `json:"limit"`
-	UID   string  `json:"uid"`
-	Cur   string  `json:"cur"`
+	BF      float64 `json:"bf"`
+	BC      float64 `json:"bc"`
+	Limit   uint    `json:"limit"`
+	UID     string  `json:"uid"`
+	Cur     string  `json:"cur"`
+	TraceID string  `json:"trace_id"`
 }
 
 type RequestV1 struct {
 	PriceFloor float64 `json:"price_floor"`
 	PriceCeil  float64 `json:"price_ceil"`
 	UserID     string  `json:"user_id"`
+	TraceID    string  `json:"trace_id"`
 }
 
 type RequestV2 struct {
 	PriceLow float32 `json:"price_low"`
 	User     string  `json:"user"`
 	Currency string  `json:"currency"`
+	TraceID  string  `json:"trace_id"`
 }
 
 type RequestV3 string
@@ -40,6 +43,7 @@ func (r *Request) FromV1(p []byte) (err error) {
 	r.Limit = 1
 	r.UID = v1.UserID
 	r.Cur = "USD"
+	r.TraceID = v1.TraceID
 	return
 }
 
@@ -53,6 +57,7 @@ func (r *Request) FromV2(p []byte) (err error) {
 	r.Limit = 1
 	r.UID = v2.User
 	r.Cur = v2.Currency
+	r.TraceID = v2.TraceID
 	return
 }
 
@@ -81,6 +86,8 @@ func (r *Request) FromV3(p []byte) (err error) {
 			r.UID = vv[0]
 		case "cur", "currency":
 			r.Cur = vv[0]
+		case "trace_id", "tid":
+			r.TraceID = vv[0]
 		}
 	}
 	if r.BC <= r.BF {
@@ -93,6 +100,7 @@ func (r Request) ToV1() []byte {
 	v1 := RequestV1{}
 	v1.PriceFloor, v1.PriceCeil = r.BF, r.BC
 	v1.UserID = r.UID
+	v1.TraceID = r.TraceID
 	b, _ := json.Marshal(v1)
 	return b
 }
@@ -102,10 +110,11 @@ func (r Request) ToV2() []byte {
 	v2.PriceLow = float32(r.BF)
 	v2.User = r.UID
 	v2.Currency = r.Cur
+	v2.TraceID = r.TraceID
 	b, _ := json.Marshal(v2)
 	return b
 }
 
 func (r Request) ToV3() []byte {
-	return []byte(fmt.Sprintf("/v3?bf=%f&bc=%f&lim=%d&user_id=%s&cur=%s", r.BF, r.BC, r.Limit, r.UID, r.Cur))
+	return []byte(fmt.Sprintf("/v3?bf=%f&bc=%f&lim=%d&user_id=%s&cur=%s&trace_id=%s", r.BF, r.BC, r.Limit, r.UID, r.Cur, r.TraceID))
 }
--- traceID/model/response.go
+++ traceID/model/response.go
@@ -3,26 +3,30 @@ package model
 import "encoding/json"
 
 type Response struct {
-	Bid  float64 `json:"bid"`
-	Cur  string  `json:"cur"`
-	Mkup string  `json:"mkup"`
+	Bid     float64 `json:"bid"`
+	Cur     string  `json:"cur"`
+	Mkup    string  `json:"mkup"`
+	TraceID string  `json:"trace_id"`
 }
 
 type ResponseV1 struct {
-	Price  float32 `json:"price"`
-	Markup []byte  `json:"markup"`
+	Price   float32 `json:"price"`
+	Markup  []byte  `json:"markup"`
+	TraceID string  `json:"trace_id"`
 }
 
 type ResponseV2 struct {
 	Commission float64 `json:"commission"`
 	Currency   string  `json:"currency"`
 	Data       string  `json:"data"`
+	TraceID    string  `json:"trace_id"`
 }
 
 type ResponseV3 struct {
-	A float32 `json:"a"`
-	B string  `json:"b"`
-	C string  `json:"c"`
+	A       float32 `json:"a"`
+	B       string  `json:"b"`
+	C       string  `json:"c"`
+	TraceID string  `json:"trace_id"`
 }
 
 func (r *Response) FromV1(p []byte) (err error) {
@@ -33,6 +37,7 @@ func (r *Response) FromV1(p []byte) (err error) {
 	r.Bid = float64(v1.Price)
 	r.Cur = "USD"
 	r.Mkup = string(v1.Markup)
+	r.TraceID = v1.TraceID
 	return
 }
 
@@ -44,6 +49,7 @@ func (r *Response) FromV2(p []byte) (err error) {
 	r.Bid = v2.Commission
 	r.Mkup = v2.Data
 	r.Cur = v2.Currency
+	r.TraceID = v2.TraceID
 	return
 }
 
@@ -55,5 +61,6 @@ func (r *Response) FromV3(p []byte) (err error) {
 	r.Bid = float64(v3.A)
 	r.Mkup = v3.B
 	r.Cur = v3.C
+	r.TraceID = v3.TraceID
 	return
 }
