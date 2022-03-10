56f43ecd5892917c634a9e093cd85a86a1aea31c
Konstantin Voykov <koykov0@gmail.com>
2022-03-10 18:31:14 +0200

    traceID: cover server with traces.

--- traceID/cmd/server/auction.go
+++ traceID/cmd/server/auction.go
@@ -8,6 +8,8 @@ import (
 	"sync"
 
 	"github.com/koykov/demo/traceID/model"
+	"github.com/koykov/fastconv"
+	"github.com/koykov/traceID"
 )
 
 type re struct {
@@ -17,11 +19,13 @@ type re struct {
 
 type streamRE chan re
 
-func Auction(req *model.Request) (resp *model.Response, err error) {
+func Auction(ttx *traceID.Ctx, req *model.Request) (resp *model.Response, err error) {
 	var pool []cv
 	if pool, err = filterClients(req); err != nil {
+		ttx.Error("build clients pool failed").Err(err)
 		return
 	}
+	ttx.Info("clients pool").Var("list", pool)
 
 	stream := make(streamRE, len(pool))
 	var (
@@ -31,7 +35,15 @@ func Auction(req *model.Request) (resp *model.Response, err error) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	go func(ctx context.Context, stream streamRE) {
+	go func(ctx context.Context, ttx *traceID.Ctx, stream streamRE) {
+		var c int
+		tth := ttx.AcquireThread()
+		defer func() {
+			ttx.Debug("reviewed {count} responses").
+				Var("count", c).
+				Var("max bid", maxBid)
+			ttx.ReleaseThread(tth)
+		}()
 		for {
 			select {
 			case <-ctx.Done():
@@ -40,65 +52,111 @@ func Auction(req *model.Request) (resp *model.Response, err error) {
 				if !ok {
 					return
 				}
+				if re.err != nil {
+					continue
+				}
+				c++
 				if re.resp.Bid > maxBid {
 					winner = re.resp
 				}
 			}
 		}
-	}(ctx, stream)
+	}(ctx, ttx, stream)
 
 	var wg sync.WaitGroup
 	for i := 0; i < len(pool); i++ {
 		wg.Add(1)
-		go execReq(&pool[i], req, stream)
+		go execReq(ttx, &pool[i], req, stream)
 	}
 	wg.Wait()
 
 	close(stream)
 
+	// todo consider bid floor/ceil
+
+	ttx.Info("auction winner").
+		Var("winner", winner)
+
 	return
 }
 
-func execReq(cv *cv, req *model.Request, stream streamRE) {
+func execReq(ttx *traceID.Ctx, cv *cv, req *model.Request, stream streamRE) {
 	var (
 		resp re
 		hr   *http.Response
 		buf  []byte
 	)
+
+	tth := ttx.AcquireThread()
+	defer ttx.ReleaseThread(tth)
+
 	defer func() { stream <- resp }()
 	switch cv.v {
 	case "v1":
 		b := req.ToV1()
+		tth.Info("send request").
+			Var("addr", cv.c).
+			Var("version", cv.v).
+			Var("body", fastconv.B2S(b))
 		if hr, resp.err = http.Post(cv.c, "application/json", bytes.NewBuffer(b)); resp.err != nil {
+			tth.Error("request failed").
+				Var("code", hr.StatusCode).
+				Err(resp.err)
 			return
 		}
 		buf, resp.err = io.ReadAll(hr.Body)
 		if resp.err != nil {
+			tth.Error("body read failed").Err(resp.err)
 			return
 		}
 		if resp.err = resp.resp.FromV1(buf); resp.err != nil {
+			tth.Error("body decoding failed").
+				Var("version", cv.v).
+				Err(resp.err)
 			return
 		}
 	case "v2":
-		b := req.ToV1()
+		b := req.ToV2()
+		tth.Info("send request").
+			Var("addr", cv.c).
+			Var("version", cv.v).
+			Var("body", fastconv.B2S(b))
 		if hr, resp.err = http.Post(cv.c, "application/json", bytes.NewBuffer(b)); resp.err != nil {
+			tth.Error("request failed").
+				Var("code", hr.StatusCode).
+				Err(resp.err)
 			return
 		}
 		if buf, resp.err = io.ReadAll(hr.Body); resp.err != nil {
+			tth.Error("body read failed").Err(resp.err)
 			return
 		}
 		if resp.err = resp.resp.FromV2(buf); resp.err != nil {
+			tth.Error("body decoding failed").
+				Var("version", cv.v).
+				Err(resp.err)
 			return
 		}
 	case "v3":
-		b := req.ToV1()
+		b := req.ToV3()
+		tth.Info("send request").
+			Var("addr", cv.c).
+			Var("version", cv.v).
+			Var("url", fastconv.B2S(b))
 		if hr, resp.err = http.Get(cv.c + string(b)); resp.err != nil {
+			tth.Error("request failed").
+				Var("code", hr.StatusCode).
+				Err(resp.err)
 			return
 		}
 		if buf, resp.err = io.ReadAll(hr.Body); resp.err != nil {
+			tth.Error("body read failed").Err(resp.err)
 			return
 		}
 		if resp.err = resp.resp.FromV3(buf); resp.err != nil {
+			tth.Error("body decoding failed").
+				Var("version", cv.v).
+				Err(resp.err)
 			return
 		}
 	}
--- traceID/cmd/server/server.go
+++ traceID/cmd/server/server.go
@@ -5,8 +5,10 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/koykov/demo/traceID"
+	td "github.com/koykov/demo/traceID"
 	"github.com/koykov/demo/traceID/model"
+	"github.com/koykov/fastconv"
+	"github.com/koykov/traceID"
 )
 
 type ServerHTTP struct{}
@@ -25,27 +27,45 @@ func (h *ServerHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		err  error
 	)
 	status := http.StatusOK
+	ttx := traceID.AcquireCtx()
 	defer func() {
 		w.WriteHeader(status)
 		_, _ = w.Write(out)
+
+		ttx.Info("response").
+			Var("status", status).
+			Var("body", fastconv.B2S(out))
+
+		_ = ttx.Flush()
+		traceID.ReleaseCtx(ttx)
 	}()
 
 	switch r.URL.Path {
 	case "/v1":
-		if !traceID.CheckMethod(r, "POST") {
+		ttx.Info("income /v1 request").
+			Var("method", r.Method).
+			Var("url", r.URL)
+		if !td.CheckMethod(r, "POST") {
+			ttx.Error("method mismatch").
+				Var("need", "POST").
+				Var("got", r.Method)
 			status = http.StatusMethodNotAllowed
 			return
 		}
 		body, err = io.ReadAll(r.Body)
 		if err != nil {
+			ttx.Error("body read error").Err(err)
 			status = http.StatusInternalServerError
 			return
 		}
+		ttx.Info("request body").Var("body", fastconv.B2S(body))
 		if err = req.FromV1(body); err != nil {
+			ttx.Error("body decoding from v1 failed").Err(err)
 			status = http.StatusBadRequest
 			return
 		}
-		if resp, err = Auction(&req); err != nil {
+		if resp, err = Auction(ttx, &req); err != nil {
+			ttx.Error("auction failed").Err(err)
 			status = http.StatusInternalServerError
 			return
 		}
@@ -53,20 +73,29 @@ func (h *ServerHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		out, _ = json.Marshal(resp)
 
 	case "/v2":
-		if !traceID.CheckMethod(r, "POST") {
+		ttx.Info("income /v1 request").
+			Var("method", r.Method).
+			Var("url", r.URL)
+		if !td.CheckMethod(r, "POST") {
+			ttx.Error("method mismatch").
+				Var("need", "POST").
+				Var("got", r.Method)
 			status = http.StatusMethodNotAllowed
 			return
 		}
 		body, err = io.ReadAll(r.Body)
 		if err != nil {
+			ttx.Error("body read error").Err(err)
 			status = http.StatusInternalServerError
 			return
 		}
 		if err = req.FromV2(body); err != nil {
+			ttx.Error("body decoding from v2 failed").Err(err)
 			status = http.StatusBadRequest
 			return
 		}
-		if resp, err = Auction(&req); err != nil {
+		if resp, err = Auction(ttx, &req); err != nil {
+			ttx.Error("auction failed").Err(err)
 			status = http.StatusInternalServerError
 			return
 		}
@@ -74,15 +103,23 @@ func (h *ServerHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		out, _ = json.Marshal(resp)
 
 	case "/v3":
-		if !traceID.CheckMethod(r, "GET") {
+		ttx.Info("income /v1 request").
+			Var("method", r.Method).
+			Var("url", r.URL)
+		if !td.CheckMethod(r, "GET") {
+			ttx.Error("method mismatch").
+				Var("need", "GET").
+				Var("got", r.Method)
 			status = http.StatusMethodNotAllowed
 			return
 		}
 		if err = req.FromV3([]byte(r.URL.RawQuery)); err != nil {
+			ttx.Error("body decoding from v3 failed").Err(err)
 			status = http.StatusBadRequest
 			return
 		}
-		if resp, err = Auction(&req); err != nil {
+		if resp, err = Auction(ttx, &req); err != nil {
+			ttx.Error("auction failed").Err(err)
 			status = http.StatusInternalServerError
 			return
 		}
