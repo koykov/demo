0d190a8855394f8c8c6d95aee290bd4eba0d7796
Konstantin Voykov <koykov0@gmail.com>
2022-05-04 16:07:29 +0300

    traceID: FS issues.

--- traceID/start.sh
+++ traceID/start.sh
@@ -4,10 +4,10 @@
 
 source .env
 
-rm -f log/*.log
-bin/trace-server -port $SERVER_PORT -tport $TRACED_PORT -cbport $CB_PORT -pbport $PB_PORT -cport $CLIENT_PORTS &> log/server.log & SERVER_PID=$!
+rm -f logs/*.log
+bin/trace-server -port $SERVER_PORT -tport $TRACED_PORT -cbport $CB_PORT -pbport $PB_PORT -cport $CLIENT_PORTS &> logs/server.log & SERVER_PID=$!
 
 IFS=',' read -ra PORTS <<< "$CLIENT_PORTS"
 for PORT in "${PORTS[@]}"; do
-  bin/trace-client -port $PORT &> "log/client-$PORT.log" & CLIENT_PID=$!
+  bin/trace-client -port $PORT &> "logs/client-$PORT.log" & CLIENT_PID=$!
 done
