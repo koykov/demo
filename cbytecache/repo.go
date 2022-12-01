5e2404851cb17b19551f243e8c8c893c0c920e00
Konstantin Voykov <koykov0@gmail.com>
2022-12-01 22:41:22 +0200

    cbytecache: add support of string intervals init.

--- cbytecache/repo.go
+++ cbytecache/repo.go
@@ -7,13 +7,16 @@ import (
 )
 
 type RequestInit struct {
-	Buckets        uint                  `json:"buckets"`
-	ExpireInterval time.Duration         `json:"expire_interval_ns"`
-	EvictInterval  time.Duration         `json:"evict_interval_ns"`
-	VacuumInterval time.Duration         `json:"vacuum_interval_ns"`
-	VacuumRatio    float64               `json:"vacuum_ratio"`
-	CollisionCheck bool                  `json:"collision_check"`
-	Capacity       cbytecache.MemorySize `json:"capacity"`
+	Buckets          uint                  `json:"buckets"`
+	ExpireIntervalNS time.Duration         `json:"expire_interval_ns"`
+	EvictIntervalNS  time.Duration         `json:"evict_interval_ns"`
+	VacuumIntervalNS time.Duration         `json:"vacuum_interval_ns"`
+	ExpireInterval   string                `json:"expire_interval"`
+	EvictInterval    string                `json:"evict_interval"`
+	VacuumInterval   string                `json:"vacuum_interval"`
+	VacuumRatio      float64               `json:"vacuum_ratio"`
+	CollisionCheck   bool                  `json:"collision_check"`
+	Capacity         cbytecache.MemorySize `json:"capacity"`
 
 	KRP uint32 `json:"krp"` // KRP - keys rotate percent
 
@@ -28,9 +31,15 @@ type RequestInit struct {
 
 func (r *RequestInit) MapConfig(conf *cbytecache.Config) {
 	conf.Buckets = r.Buckets
-	conf.ExpireInterval = r.ExpireInterval
-	conf.EvictInterval = r.EvictInterval
-	conf.VacuumInterval = r.VacuumInterval
+	if conf.ExpireInterval = r.ExpireIntervalNS; conf.ExpireInterval == 0 {
+		conf.ExpireInterval, _ = time.ParseDuration(r.ExpireInterval)
+	}
+	if conf.EvictInterval = r.EvictIntervalNS; conf.EvictInterval == 0 {
+		conf.EvictInterval, _ = time.ParseDuration(r.EvictInterval)
+	}
+	if conf.VacuumInterval = r.VacuumIntervalNS; conf.VacuumInterval == 0 {
+		conf.VacuumInterval, _ = time.ParseDuration(r.VacuumInterval)
+	}
 	conf.VacuumRatio = r.VacuumRatio
 	conf.CollisionCheck = r.CollisionCheck
 	conf.Capacity = r.Capacity
