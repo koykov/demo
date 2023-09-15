a10217aaa5ba8d462249fd2f0aafe6cb26601d4b
Konstantin Voykov <koykov0@gmail.com>
2023-09-15 19:38:34 +0300

    batch_query: complete record matcher.

--- batch_query/sql.go
+++ batch_query/sql.go
@@ -1,6 +1,9 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"strconv"
+)
 
 type SQLRecord struct {
 	ID      int32
@@ -32,10 +35,40 @@ func (r SQLRecord) Match(key, value any) bool {
 
 	var id int32
 	switch key.(type) {
+	case int:
+		id = int32(key.(int))
+	case int8:
+		id = int32(key.(int8))
+	case int16:
+		id = int32(key.(int16))
 	case int32:
 		id = key.(int32)
-
-		// ...
+	case int64:
+		id = int32(key.(int64))
+	case uint:
+		id = int32(key.(uint))
+	case uint8:
+		id = int32(key.(uint8))
+	case uint16:
+		id = int32(key.(uint16))
+	case uint32:
+		id = int32(key.(uint32))
+	case uint64:
+		id = int32(key.(uint64))
+	case string:
+		id64, err := strconv.ParseInt(key.(string), 10, 64)
+		if err != nil {
+			return false
+		}
+		id = int32(id64)
+	case []byte:
+		id64, err := strconv.ParseInt(string(key.([]byte)), 10, 64)
+		if err != nil {
+			return false
+		}
+		id = int32(id64)
+	default:
+		return false
 	}
 
 	return rec.ID == id
