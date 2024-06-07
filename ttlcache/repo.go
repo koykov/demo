d95fc258ec30833244b0835ea5d4f236f516ad67
Konstantin Voykov <koykov0@gmail.com>
2024-06-07 21:11:48 +0300

    ttlcache: extend init request with dump fields

--- ttlcache/repo.go
+++ ttlcache/repo.go
@@ -7,11 +7,16 @@ import (
 )
 
 type RequestInit struct {
-	Buckets         uint          `json:"buckets"`
-	Size            uint64        `json:"size"`
-	TTLIntervalNS   time.Duration `json:"ttl_interval_ns"`
-	EvictIntervalNS time.Duration `json:"evict_interval_ns"`
-	EvictWorkers    uint          `json:"evict_workers"`
+	Buckets          uint          `json:"buckets"`
+	Size             uint64        `json:"size"`
+	TTLIntervalNS    time.Duration `json:"ttl_interval_ns"`
+	EvictIntervalNS  time.Duration `json:"evict_interval_ns"`
+	EvictWorkers     uint          `json:"evict_workers"`
+	DumpIntervalNS   time.Duration `json:"dump_interval_ns"`
+	DumpWriteWorkers uint          `json:"dump_write_workers"`
+	DumpReadBuffer   uint          `json:"dump_read_buffer"`
+	DumpReadWorkers  uint          `json:"dump_read_workers"`
+	DumpReadAsync    bool          `json:"dump_read_async"`
 
 	DeletePercent uint `json:"delete_percent"`
 
@@ -30,6 +35,11 @@ func (r *RequestInit) MapConfig(conf *ttlcache.Config[entry]) {
 	conf.TTLInterval = r.TTLIntervalNS
 	conf.EvictInterval = r.EvictIntervalNS
 	conf.EvictWorkers = r.EvictWorkers
+	conf.DumpInterval = r.DumpIntervalNS
+	conf.DumpWriteWorkers = r.DumpWriteWorkers
+	conf.DumpReadBuffer = r.DumpReadBuffer
+	conf.DumpReadWorkers = r.DumpReadWorkers
+	conf.DumpReadAsync = r.DumpReadAsync
 	if r.DeletePercent > 100 {
 		r.DeletePercent = 0
 	}
